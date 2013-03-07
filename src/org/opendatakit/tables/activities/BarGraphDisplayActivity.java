9de6bfe036298c95abdc342ddfbc18c83927d3a3
Nathan Brandes <Nathan Brandes@NathanBrandes.dhcp4.washington.edu>
2013-03-07 10:46:05 -0800

    Backed out merge changeset: 260337e6edc8
    Backed out merge revision to its second parent (463a9162589e)

--- src/org/opendatakit/tables/activities/BarGraphDisplayActivity.java
+++ src/org/opendatakit/tables/activities/BarGraphDisplayActivity.java
@@ -19,6 +19,7 @@ import org.opendatakit.tables.data.DataManager;
 import org.opendatakit.tables.data.DbHelper;
 import org.opendatakit.tables.data.KeyValueStore;
 import org.opendatakit.tables.data.KeyValueStoreHelper;
+import org.opendatakit.tables.data.KeyValueStoreHelper.AspectHelper;
 import org.opendatakit.tables.data.Query;
 import org.opendatakit.tables.data.UserTable;
 import org.opendatakit.tables.view.custom.CustomGraphView;
@@ -68,8 +69,13 @@ public class BarGraphDisplayActivity extends SherlockActivity
 	        table = c.getIsOverview() ?
 	                c.getDbTable().getUserOverviewTable(query) :
 	                c.getDbTable().getUserTable(query);
-	        String filename = kvsh.getString(ListDisplayActivity.KEY_FILENAME);
-	        view = CustomGraphView.get(this, c.getTableProperties(), table, filename);
+	        String nameOfView = 
+	            kvsh.getString(ListDisplayActivity.KEY_LIST_VIEW_NAME);
+	        AspectHelper aspectHelper = kvsh.getAspectHelper(nameOfView);
+	        String filename = 
+	            aspectHelper.getString(ListDisplayActivity.KEY_FILENAME);
+	        view = CustomGraphView.get(this, c.getTableProperties(), table,
+	                filename);
 	        displayView();
 	    }
 	    
@@ -117,4 +123,99 @@ public class BarGraphDisplayActivity extends SherlockActivity
 	        c.recordSearch();
 	        init();
 	    }
+	/*
+
+    private static final int RCODE_ODKCOLLECT_ADD_ROW =
+        Controller.FIRST_FREE_RCODE;
+    
+    private DataManager dm;
+    private Controller c;
+    private Query query;
+    private List<String> labels;
+    private List<Double> values;
+    private boolean yIsCount;
+    private UserTable table;
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        c = new Controller(this, this, getIntent().getExtras());
+        dm = new DataManager(DbHelper.getDbHelper(this));
+        query = new Query(dm.getAllTableProperties(KeyValueStore.Type.ACTIVE), 
+            c.getTableProperties());
+        init();
+    }
+    
+    @Override
+    public void init() {
+        query.clear();
+        query.loadFromUserQuery(c.getSearchText());
+        table = c.getIsOverview() ?
+                c.getDbTable().getUserOverviewTable(query) :
+                c.getDbTable().getUserTable(query);
+        view = CustomTableView.get(this, c.getTableProperties(), table,
+                c.getTableViewSettings().getCustomListFilename());
+        displayView();
+    }
+    
+    private void openCollectionView(int rowNum) {
+        query.clear();
+        query.loadFromUserQuery(c.getSearchText());
+        query.addConstraint(c.getTableViewSettings().getBarXCol(),
+                labels.get(rowNum));
+        Controller.launchTableActivity(this, c.getTableProperties(),
+                query.toUserQuery(), false);
+    }
+    
+    @Override
+    public void onBackPressed() {
+        c.onBackPressed();
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+            Intent data) {
+        if (c.handleActivityReturn(requestCode, resultCode, data)) {
+            return;
+        }
+        switch (requestCode) {
+        case RCODE_ODKCOLLECT_ADD_ROW:
+            c.addRowFromOdkCollectForm(
+                    Integer.valueOf(data.getData().getLastPathSegment()));
+            init();
+            break;
+        default:
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        c.buildOptionsMenu(menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        return c.handleMenuItemSelection(item);
+    }
+    
+    @Override
+    public void onSearch() {
+        c.recordSearch();
+        init();
+    }
+    
+    private class BarChartListener implements BarChart.ClickListener {
+        
+        @Override
+        public void onClick(int index) {
+            if (yIsCount) {
+                openCollectionView(index);
+            } else {
+                Controller.launchDetailActivity(BarGraphDisplayActivity.this,
+                        c.getTableProperties(), table, index);
+            }
+        }
+    }*/
 }
