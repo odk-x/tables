f19c6f5acca672b8dd75c9ceee7868576d9bcefc
Sam Sudar <sudar.sam@gmail.com>
2014-04-22 17:06:13 -0700

    changed back to old javascript api. Static tests pass on device. Haven't tried collect/survey tests.

--- src/org/opendatakit/tables/views/webkits/ControlIf.java
+++ src/org/opendatakit/tables/views/webkits/ControlIf.java
@@ -28,7 +28,7 @@ import android.util.Log;
  *
  */
 public class ControlIf {
-
+  
   private static final String TAG = ControlIf.class.getSimpleName();
 
 	private WeakReference<Control> weakControl;
@@ -38,158 +38,108 @@ public class ControlIf {
 	}
 
 	/**
-	 * Open the table with the given tableId.
-	 * This opens the full table, without any where filter, group-bys or ordering.
-	 *
-	 * @param tableId
-    * @return true if the open succeeded
+	 * Open the table with the given id.
+	 * @see #query(String, String, String[])
+	 * @param tableId the table id of the table to open
+	 * @param whereClause query as specified by 
+	 * {@link #query(String, String, String[])}
+	 * If null will not restrict the results.
+	 * @param selectionArgs an array of selection arguments, one for each "?"
+	 * in whereClause. If null will not restrict the results.
+	 * @return true if the open succeeded
 	 */
-   // @JavascriptInterface
-   public boolean openTable(String tableId) {
-     // TODO: convert to element keys
-     Log.e(TAG, "TO-WC openTableWithSqlQuery(tableId, sqlWhereClause, " +
-         "sqlSelectionArgs");
-      return weakControl.get().helperOpenTable(tableId,
-            null, null, null, null, null, null);
-   }
-
-   /**
-    * Open the table with the given tableId.
-    * Applies the specified where, group by and ordering clauses.
-    * Only the tableId is required to be non-null.
-    *
-    * @param tableId
-    * @param whereClause
-    * @param selectionArgs
-    * @param groupBy
-    * @param having
-    * @param orderByElementKey
-    * @param orderByDirection
-    * @return
-    */
 	// @JavascriptInterface
-	public boolean openTableWithSql(String tableId,
-			String whereClause, String[] selectionArgs,
-			String[] groupBy, String having, String orderByElementKey, String orderByDirection) {
+	public boolean openTable(String tableId,
+			String whereClause, String[] selectionArgs) {
 	  // TODO: convert to element keys
 	  Log.e(TAG, "TO-WC openTableWithSqlQuery(tableId, sqlWhereClause, " +
 	  		"sqlSelectionArgs");
-		return weakControl.get().helperOpenTable(tableId,
-				whereClause, selectionArgs, groupBy, having, orderByElementKey, orderByDirection);
+		return weakControl.get().helperOpenTable(
+		    tableId,
+		    whereClause,
+		    selectionArgs,
+		    null,
+		    null,
+		    null,
+		    null);
 	}
-
+	
    /**
-    * Opens the given tableId to a list view.
-    * Applies no where filter, group-by or ordering clauses.
-    * If the relativePath is null, it opens to the ListManager.
-    *
-    * @param tableId
-    * @param relativePath
-    * @return
+    * Open the given table with the given list view, restricted by given query.
+    * @see #query(String, String, String[])
+    * @see #openTable(String, String, String[])
+    * @param tableId the tableId of the table to open
+    * @param whereClause query as specified by 
+    * {@link #query(String, String, String[])}
+    * If null will not restrict the results.
+    * @param selectionArgs an array of selection arguments, one for each "?"
+    * in whereClause. If null will not restrict the results.
+    * @param relativePath the name of the file specifying the list view,
+    * relative to the app folder.
+    * @return true if the open succeeded
     */
-   // @JavascriptInterface
-   public boolean openTableToListView(String tableId, String relativePath) {
+	public boolean openTableToListView(String tableId, String whereClause,
+	    String[] selectionArgs, String relativePath) {
      return weakControl.get().helperOpenTableWithFile(
-         tableId, relativePath, null, null, null, null, null, null);
-   }
-
-	/**
-	 * Opens the given tableId to a list view.
-	 * If the relativePath is null, it uses the default list view for the
-	 * tableId. If that is not set, then it opens to the ListViewManager.
-	 *
-	 * All other parameters can be null.
-	 *
-	 * @param tableId
-	 * @param relativePath
-	 * @param whereClause
-	 * @param selectionArgs
-	 * @param groupBy
-	 * @param having
-	 * @param orderByElementKey
-	 * @param orderByDirection
-	 * @return
-	 */
-   // @JavascriptInterface
-	public boolean openTableToListViewWithSql(String tableId, String relativePath,
-       String whereClause, String[] selectionArgs,
-       String[] groupBy, String having, String orderByElementKey, String orderByDirection) {
-     return weakControl.get().helperOpenTableWithFile(
-         tableId, relativePath, whereClause, selectionArgs, groupBy, having, orderByElementKey, orderByDirection);
+         tableId,
+         relativePath,
+         whereClause,
+         selectionArgs,
+         null,
+         null,
+         null,
+         null);
 	}
-
-	/**
-    * Opens the given tableId to a map view with the specified list view.
-    * If the relativePath is null, it uses the default map list view for the
-    * tableId. If that default is not set, then the map view is still displayed,
-    * a toast shows, and there is no list view available.
-	 *
-	 * @param tableId
-	 * @param relativePath
-	 * @return
-	 */
-   // @JavascriptInterface
-   public boolean openTableToMapView(String tableId, String relativePath) {
-     return weakControl.get().helperOpenTableToMapView(
-         tableId, relativePath, null, null, null, null, null, null);
-   }
-
+	
    /**
-    * Opens the given tableId to a map view with the specified list view.
-    * If the relativePath is null, it uses the default map list view for the
-    * tableId. If that default is not set, then the map view is still displayed,
-    * a toast shows, and there is no list view available.
-    *
-    * All other parameters can be null.
-    *
-    * @param tableId
-    * @param relativePath
-    * @param whereClause
-    * @param selectionArgs
-    * @param groupBy
-    * @param having
-    * @param orderByElementKey
-    * @param orderByDirection
-    * @return
+    * Open the given table to the map view, restricted with the given SQL
+    * query.
+    * @see #query(String, String, String[])
+    * @see #openTable(String, String, String[])
+    * @param tableId the tableId of the table to open
+    * @param whereClause query as specified by 
+    * {@link #query(String, String, String[])}
+    * If null will not restrict the results.
+    * @param selectionArgs an array of selection arguments, one for each "?"
+    * in whereClause. If null will not restrict the results.
+    * @param relativePath NOT YET SUPPORTED
+    * @return true if the open succeeded
     */
-   // @JavascriptInterface
-	public boolean openTableToMapViewWithSql(String tableId, String relativePath,
-       String whereClause, String[] selectionArgs,
-       String[] groupBy, String having, String orderByElementKey, String orderByDirection) {
+	public boolean openTableToMapView(String tableId, String whereClause,
+	    String[] selectionArgs, String relativePath) {
      return weakControl.get().helperOpenTableToMapView(
-         tableId, relativePath, whereClause, selectionArgs, groupBy, having, orderByElementKey, orderByDirection);
+         tableId,
+         relativePath,
+         whereClause,
+         selectionArgs,
+         null,
+         null,
+         null,
+         null);
 	}
-
-	/**
-    * Open the spreadsheet view of the given tableId.
-	 *
-	 * @param tableId
-	 * @return
-	 */
-   // @JavascriptInterface
-   public boolean openTableToSpreadsheetView(String tableId) {
-     return weakControl.get().helperOpenTableToSpreadsheetView(
-         tableId, null, null, null, null, null, null);
-   }
-
+	
    /**
     * Open the table to spreadsheet view, restricting by the given SQL query.
     * @see #query(String, String, String[])
     * @see #openTable(String, String, String[])
     * @param tableId the tableId of the table to open
-    * @param whereClause query as specified by
+    * @param whereClause query as specified by 
     * {@link #query(String, String, String[])}
     * If null will not restrict the results.
     * @param selectionArgs an array of selection arguments, one for each "?"
     * in whereClause. If null will not restrict the results.
     * @return true if the open succeeded
     */
-   // @JavascriptInterface
-	public boolean openTableToSpreadsheetViewWithSql(String tableId,
-       String whereClause, String[] selectionArgs,
-       String[] groupBy, String having, String orderByElementKey, String orderByDirection) {
+	public boolean openTableToSpreadsheetView(String tableId, 
+	    String whereClause, String[] selectionArgs) {
      return weakControl.get().helperOpenTableToSpreadsheetView(
-         tableId, whereClause, selectionArgs, groupBy, having, orderByElementKey, orderByDirection);
+         tableId,
+         whereClause,
+         selectionArgs,
+         null,
+         null,
+         null,
+         null);
 	}
 
 	/**
@@ -197,7 +147,7 @@ public class ControlIf {
 	 * rows have been restricted by the query.
 	 * <p>
 	 * For example, if you wanted all the rows where the column with elementKey
-	 * foo equaled bar, the where clause would be "foo = ? ", and the selection
+	 * foo equaled bar, the where clause would be "foo = ? ", and the selection 
 	 * args would be ["bar"].
 	 * <p>
 	 * If you require only those rows where foo equals bar and foo2 = bar2,
@@ -219,7 +169,7 @@ public class ControlIf {
 	 * <p>
 	 * This can be used to do powerful cross-table queries.
 	 * @param tableId the tableId of the table
-	 * @param whereClause a where clause as described above. Must include "?"
+	 * @param whereClause a where clause as described above. Must include "?" 
 	 * instead of actual values, which are instead
     * passed in the sqlSelectionArgs parameter. The references to tables must
     * use the table ids. The references to the columns
@@ -232,9 +182,15 @@ public class ControlIf {
 	 */
 	// @JavascriptInterface
 	public TableDataIf query(String tableId, String whereClause,
-			String[] selectionArgs, String[] groupBy, String having, String orderByElementKey, String orderByDirection) {
-		TableData td = weakControl.get().query(tableId, whereClause,
-				selectionArgs, groupBy, having, orderByElementKey, orderByDirection);
+			String[] selectionArgs) {
+		TableData td = weakControl.get().query(
+		    tableId,
+		    whereClause,
+		    selectionArgs,
+		    null,
+		    null,
+		    null,
+		    null);
 		if (td != null) {
 			return td.getJavascriptInterfaceWithWeakReference();
 		} else {
@@ -291,9 +247,9 @@ public class ControlIf {
 		return weakControl.get().openDetailViewWithFile(tableId, rowId,
 		    relativePath);
 	}
-
+	
 	/**
-	 * Add a row using Collect and the default form.
+	 * Add a row using Collect and the default form. 
 	 * @param tableId the tableId of the table to receive the add.
     * @return true if the activity was launched, false if something went wrong
     * @deprecated
@@ -301,10 +257,10 @@ public class ControlIf {
 	// @JavascriptInterface
 	public boolean addRowWithCollectDefault(String tableId) {
 	  Log.e(TAG, "TO-WC addRowWithCollect");
-		return this.addRowWithCollect(tableId, null, null,
+		return this.addRowWithCollect(tableId, null, null, 
 		    null, null);
 	}
-
+   
   /**
    * Add a row using Collect, a specific form, and a map of prepopulated
    * values.
@@ -322,15 +278,14 @@ public class ControlIf {
    * @return true if the activity was launched, false if something went wrong
    * @deprecated
    */
-   // @JavascriptInterface
    public boolean addRowWithCollect(String tableId,
-       String formId, String formVersion, String formRootElement,
+       String formId, String formVersion, String formRootElement, 
        String jsonMap) {
      return weakControl.get()
-         .helperAddRowWithCollect(tableId, formId, formVersion,
+         .helperAddRowWithCollect(tableId, formId, formVersion, 
              formRootElement, jsonMap);
    }
-
+   
    /**
     * Edit the given row using Collect.
     * @param tableId
@@ -338,11 +293,10 @@ public class ControlIf {
     * @return true if the activity was launched, false if something went wrong
     * @deprecated
     */
-   // @JavascriptInterface
    public boolean editRowWithCollectDefault(String tableId, String rowId) {
      return this.editRowWithCollect(tableId, rowId, null, null, null);
    }
-
+   
    /**
     * Edit the given row using Collect and a specific form.
     * @param tableId
@@ -353,26 +307,24 @@ public class ControlIf {
     * @return true if the activity was launched, false if something went wrong
     * @deprecated
     */
-   // @JavascriptInterface
-   public boolean editRowWithCollect(String tableId,
-       String rowId, String formId, String formVersion,
+   public boolean editRowWithCollect(String tableId, 
+       String rowId, String formId, String formVersion, 
        String formRootElement) {
      return weakControl.get().helperEditRowWithCollect(tableId, rowId, formId,
          formVersion, formRootElement);
    }
 
-
+   
    /**
     * Edit the given row using Survey and the default form.
     * @param tableId
     * @param rowId
     * @return true if the activity was launched, false if something went wrong
     */
-   // @JavascriptInterface
    public boolean editRowWithSurveyDefault(String tableId, String rowId) {
      return editRowWithSurvey(tableId, rowId, null, null);
    }
-
+   
    /**
     * Edit the given row using Survey and a specific form.
     * @param tableId
@@ -381,39 +333,36 @@ public class ControlIf {
     * @param screenPath
     * @return true if the activity was launched, false if something went wrong
     */
-   // @JavascriptInterface
    public boolean editRowWithSurvey(String tableId, String rowId,
        String formId, String screenPath) {
-     return weakControl.get().helperEditRowWithSurvey(tableId, rowId, formId,
+     return weakControl.get().helperEditRowWithSurvey(tableId, rowId, formId, 
          screenPath);
    }
-
+   
    /**
     * Add a row with Survey and the default form.
     * @param tableId the table to receive the add
     * @return true if Survey was launched, else false
     */
-   // @JavascriptInterface
    public boolean addRowWithSurveyDefault(String tableId) {
      return this.addRowWithSurvey(tableId, null, null, null);
    }
-
+   
    /**
-    * Add a row using Survey.
+    * Add a row using Survey. 
     * @param tableId
     * @param formId if null, the default form will be used
     * @param screenPath
-    * @param jsonMap a stringified json object matching element key to
+    * @param jsonMap a stringified json object matching element key to 
     * the value to prepopulate in the new row
     * @return true if the activity was launched, false if something went wrong
     */
-   // @JavascriptInterface
-   public boolean addRowWithSurvey(String tableId, String formId,
+   public boolean addRowWithSurvey(String tableId, String formId, 
        String screenPath, String jsonMap) {
      return weakControl.get().helperAddRowWithSurvey(
          tableId, formId, screenPath, jsonMap);
    }
-
+	
 	/**
 	 * Return the element key for the column with the given element path.
 	 * @param tableId
@@ -421,22 +370,20 @@ public class ControlIf {
 	 * @return the element key for the column, or null if a table cannot be
 	 * found with the existing tableId.
 	 */
-   // @JavascriptInterface
 	public String getElementKey(String tableId, String elementPath) {
 	  return weakControl.get().getElementKey(tableId, elementPath);
 	}
-
+	
 	/**
 	 * Get the display name for the given column.
 	 * @param tableId
 	 * @param elementPath
 	 * @return the display name for the given column
 	 */
-   // @JavascriptInterface
 	public String getColumnDisplayName(String tableId, String elementPath) {
 	  return weakControl.get().getColumnDisplayName(tableId, elementPath);
 	}
-
+	
 	/**
 	 * Retrieve the display name for the given table.
 	 * <p>
@@ -446,11 +393,10 @@ public class ControlIf {
 	 * @return the display name for the table, in stringified json form if the
 	 * name has been internationalized
 	 */
-   // @JavascriptInterface
 	public String getTableDisplayName(String tableId) {
 	  return weakControl.get().getTableDisplayName(tableId);
 	}
-
+	
 	/**
 	 * Determine if the column exist in the given table.
 	 * @param tableId
@@ -458,28 +404,25 @@ public class ControlIf {
 	 * @return true if the column exists, else false. Returns false also if the
 	 * tableId does not match any table.
 	 */
-   // @JavascriptInterface
 	public boolean columnExists(String tableId, String elementPath) {
 	  return weakControl.get().columnExists(tableId, elementPath);
 	}
-
+	
 	/**
 	 * Take the path of a file relative to the app folder and return a url by
 	 * which it can be accessed.
 	 * @param relativePath
 	 * @return an absolute URI to the file
 	 */
-   // @JavascriptInterface
 	public String getFileAsUrl(String relativePath) {
 	  return weakControl.get().getFileAsUrl(relativePath);
 	}
-
+	
 	/**
 	 * Return the platform info as a stringified json object. This is an object
 	 * containing the keys: container, version, appName, baseUri, logLevel.
 	 * @return a stringified json object with the above keys
 	 */
-   // @JavascriptInterface
 	public String getPlatformInfo() {
 	  return weakControl.get().getPlatformInfo();
 	}
