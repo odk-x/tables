c0ef4af2ed5a842927b7da8dd0240f5d3ab78588
clarlars <clarlars@gmail.com>
2015-10-02 18:44:50 -0700

    Implement color rules in Tables

--- tables_app/src/main/java/org/opendatakit/tables/views/webkits/TableDataExecutorProcessor.java
+++ tables_app/src/main/java/org/opendatakit/tables/views/webkits/TableDataExecutorProcessor.java
@@ -14,11 +14,19 @@
 
 package org.opendatakit.tables.views.webkits;
 
+import android.os.RemoteException;
+import org.opendatakit.common.android.data.ColorGuide;
+import org.opendatakit.common.android.data.ColorRuleGroup;
+import org.opendatakit.common.android.data.RowColorObject;
 import org.opendatakit.common.android.data.UserTable;
 import org.opendatakit.common.android.views.ExecutorContext;
 import org.opendatakit.common.android.views.ExecutorProcessor;
 import org.opendatakit.database.service.KeyValueStoreEntry;
+import org.opendatakit.database.service.OdkDbHandle;
+import org.opendatakit.tables.application.Tables;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -27,12 +35,79 @@ import java.util.Map;
  */
 public class TableDataExecutorProcessor extends ExecutorProcessor {
 
+  enum colorRuleType {
+    TABLE,
+    COLUMN,
+    STATUS
+  };
+
   public TableDataExecutorProcessor(ExecutorContext context) {
     super(context);
   }
 
   @Override
-  protected void extendQueryMetadata(List<KeyValueStoreEntry> entries, UserTable userTable, Map<String, Object> metadata) {
+  protected void extendQueryMetadata(OdkDbHandle db, List<KeyValueStoreEntry> entries, UserTable userTable, Map<String, Object> metadata) {
     // TODO: construct color rule data here...
+    String [] adminCols = ADMIN_COLUMNS.toArray(new String[0]);
+
+
+    ArrayList<RowColorObject> rowColors = new ArrayList<RowColorObject>();
+    ArrayList<RowColorObject> statusColors = new ArrayList<RowColorObject>();
+    HashMap<String, ArrayList<RowColorObject>> colColors = new HashMap<String, ArrayList<RowColorObject>>();
+
+    try {
+      // Need to get the tables color rules and determine which rows are affected
+      constructRowColorObjects(db, userTable, adminCols, rowColors, colorRuleType.TABLE, null);
+
+      // Need to get the status color rules and determine which rows are affected
+      constructRowColorObjects(db, userTable, adminCols, statusColors, colorRuleType.STATUS, null);
+
+      // Need to get column color rules working
+      Map<String, Integer> elementKeyMap = (Map<String, Integer>) metadata.get("elementKeyMap");
+      for (String elementKey : elementKeyMap.keySet()) {
+        ArrayList<RowColorObject> colColorGuide = new ArrayList<RowColorObject>();
+        constructRowColorObjects(db, userTable, adminCols, colColorGuide, colorRuleType.COLUMN, elementKey);
+        if (colColorGuide.size() > 0) {
+          colColors.put(elementKey, colColorGuide);
+        }
+      }
+
+    } catch (RemoteException e) {
+      e.printStackTrace();
+    }
+
+    metadata.put("rowColors", rowColors);
+    metadata.put("statusColors", statusColors);
+    metadata.put("columnColors", colColors);
+
+  }
+
+  private void constructRowColorObjects(OdkDbHandle db, UserTable userTable, String[] adminCols, ArrayList<RowColorObject>colors, colorRuleType crType, String elementKey) throws RemoteException {
+    // Should reuse this code for column and status color rules
+
+    ColorRuleGroup crg = null;
+    // Loop through the rows
+    for (int i = 0; i < userTable.getNumberOfRows(); i++) {
+      // Get the table color rules and determine which rows are affected
+      if (crType == colorRuleType.TABLE) {
+        crg = ColorRuleGroup.getTableColorRuleGroup(Tables.getInstance(), userTable.getAppName(), db, userTable.getTableId(), adminCols);
+      } else if (crType == colorRuleType.COLUMN) {
+        crg = ColorRuleGroup.getColumnColorRuleGroup(Tables.getInstance(), userTable.getAppName(), db, userTable.getTableId(), elementKey, adminCols);
+      } else if (crType == colorRuleType.STATUS) {
+        crg = ColorRuleGroup.getStatusColumnRuleGroup(Tables.getInstance(), userTable.getAppName(), db, userTable.getTableId(), adminCols);
+
+      } else {
+        return;
+      }
+
+      ColorGuide tcg = crg.getColorGuide(userTable.getColumnDefinitions(), userTable.getRowAtIndex(i));
+
+      if (tcg != null) {
+        String hexFgString = "#" + Integer.toHexString(0x00FFFFFF & tcg.getForeground());
+        String hexBgString = "#" + Integer.toHexString(0x00FFFFFF & tcg.getBackground());
+        RowColorObject rco = new RowColorObject(userTable.getRowAtIndex(i).getRowId(), i, hexFgString, hexBgString);
+        colors.add(rco);
+      }
+    }
   }
 }
