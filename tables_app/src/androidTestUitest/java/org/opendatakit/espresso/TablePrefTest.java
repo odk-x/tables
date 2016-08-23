19dc41793559a2b8c88eb54482062aba9e9e680e
linl33 <linlee1997@gmail.com>
2016-08-23 16:26:53 -0700

    attempt to fix test failure on emulator

--- tables_app/src/androidTestUitest/java/org/opendatakit/espresso/TablePrefTest.java
+++ tables_app/src/androidTestUitest/java/org/opendatakit/espresso/TablePrefTest.java
@@ -85,6 +85,9 @@ public class TablePrefTest {
 
     //open table manager
     onView(withId(R.id.menu_web_view_activity_table_manager)).perform(click());
+    try {
+      Thread.sleep(3000);
+    } catch (Exception e) {}
 
     //click "Tea Houses Editable"
     onData(ODKMatchers.withTable(T_HOUSE_E_TABLE_ID)).perform(click());
