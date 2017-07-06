ece39be69f1658adc591348847b1a54f91180341
Niles Rogoff <me@niles.xyz>
2017-07-06 12:21:12 -0700

    Uitests still broken

--- tables_app/src/androidTestUitest/java/org/opendatakit/espresso/AbsBaseTest.java
+++ tables_app/src/androidTestUitest/java/org/opendatakit/espresso/AbsBaseTest.java
@@ -10,7 +10,7 @@ import java.lang.Thread; import java.lang.Throwable;
 import android.util.Log;
 
 public class AbsBaseTest {
-  public static Tables c;
+  public static Tables c = null;
   private static String TAG = AbsBaseTest.class.getSimpleName();
   @Before public void _setUpC() {
     try {Thread.sleep(1000);} catch (Throwable ignored) {};
@@ -20,12 +20,13 @@ public class AbsBaseTest {
     } else if (context instanceof Activity) {
       c = (Tables) ((Activity) context).getApplication();
     } else {
-      if (c == null) {
+      if (context == null) {
         Log.e(TAG, "context is null!");
       } else {
         Log.i(TAG, context.toString());
       }
-      throw new java.lang.IllegalStateException();
+      if (c == null)
+        throw new java.lang.IllegalStateException();
     }
   }
 
